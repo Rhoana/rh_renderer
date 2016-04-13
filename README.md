0fe138873b5120704ff8cb89e209d864c838d9cd
Adi Suissa-Peleg <adisuis@seas.harvard.edu>
2016-04-13 10:27:35 -0400

    Adding an initial version of the renderer that supports both affine and non-affine transformations

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # rh_renderer
-Rendering library
+A Rhoana library that performs the rendering of images after applying both affine and non-affine transformations.
+The available transformations are part of the models defined in this library.
+This package also renders tilespecs.
+
+## Requirements
+- Requires OpenCV 2.4+
+- Python requirements in [requirements.txt](requirements.txt)
+
+## Installation
+TODO.
+
+
+## Usage
+
+TODO.
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup, find_packages
+
+VERSION = 0.0.1
+version = os.path.join('rh_renderer', '__init__.py')
+execfile(version)
+
+README = open('README.md').read()
+
+setup(
+    name='rh_renderer',
+    version=VERSION,
+    packages=find_packages(),
+    package_data={'mbeam': ['web/*.*']},
+    author='Adi Suissa-Peleg',
+    author_email='adisuis@seas.harvard.edu',
+    url="https://github.com/Rhoana/rh_renderer",
+    description="Rhoana's rendering library",
+    long_description=README,
+    include_package_data=True,
+    install_requires=[
+        "numpy>=1.9.3",
+        "scipy>=0.16.0",
+    ],
+    zip_safe=False
+)
